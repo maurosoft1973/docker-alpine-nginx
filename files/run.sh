aa9bab9978a064ef2bd621d5c0d8276fd3326335
Mauro Cardillo <mauro.cardillo@gmail.com>
2020-03-31 20:30:36 +0200

    Update Script Check User

--- files/run.sh
+++ files/run.sh
@@ -4,10 +4,16 @@ WWW_USER=${WWW_USER:-"www"}
 WWW_USER_UID=${WWW_USER_UID:-"5001"}
 LOCALTIME=${LOCALTIME:-"Europe/Brussels"}
 
-echo "Create User $WWW_USER with uid $WWW_USER_UID"
-adduser -s /bin/false -H -u ${WWW_USER_UID} -D ${WWW_USER}
+#Create User (if not exist)
+CHECK=$(cat /etc/passwd | grep $WWW_USER | wc -l)
+if [ $CHECK == "0" ]; then
+    echo "Create User $WWW_USER with uid $WWW_USER_UID"
+    adduser -s /bin/false -H -u ${WWW_USER_UID} -D ${WWW_USER}
+else
+    echo -e "Skipping,user $WWW_USER exist"
+fi
 
-echo "Use user $WWW_USER for nginx process"
+echo "Change user for nginx process at $WWW_USER"
 sed "s/user {user}/user ${WWW_USER}/g" /etc/nginx/nginx.conf > /tmp/nginx.conf
 cp /tmp/nginx.conf /etc/nginx/nginx.conf
 
