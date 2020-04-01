2f3b466f2465ab9b6d5381c5781e24875ecbf87c
Mauro Cardillo <mauro.cardillo@gmail.com>
2020-04-01 19:08:16 +0200

    Update

--- files/run.sh
+++ files/run.sh
@@ -18,9 +18,17 @@ sed "s/user {user}/user ${WWW_USER}/g" /etc/nginx/nginx.conf > /tmp/nginx.conf
 cp /tmp/nginx.conf /etc/nginx/nginx.conf
 
 echo "Download Geo DB"
-wget https://gitlab.com/maurosoft1973-docker/alpine-nginx/-/raw/master/conf/etc/nginx/geoip2/GeoLite2-ASN.mmdb -O /etc/nginx/geoip2/GeoLite2-ASN.mmdb
-wget https://gitlab.com/maurosoft1973-docker/alpine-nginx/-/raw/master/conf/etc/nginx/geoip2/GeoLite2-City.mmdb -O /etc/nginx/geoip2/GeoLite2-City.mmdb
-wget https://gitlab.com/maurosoft1973-docker/alpine-nginx/-/raw/master/conf/etc/nginx/geoip2/GeoLite2-Country.mmdb -O /etc/nginx/geoip2/GeoLite2-Country.mmdb
+if [ -f "/etc/nginx/geoip2/GeoLite2-ASN.mmdb" ]; then
+    wget https://gitlab.com/maurosoft1973-docker/alpine-nginx/-/raw/master/conf/etc/nginx/geoip2/GeoLite2-ASN.mmdb -O /etc/nginx/geoip2/GeoLite2-ASN.mmdb
+fi
+
+if [ -f "/etc/nginx/geoip2/GeoLite2-City.mmdb" ]; then
+    wget https://gitlab.com/maurosoft1973-docker/alpine-nginx/-/raw/master/conf/etc/nginx/geoip2/GeoLite2-City.mmdb -O /etc/nginx/geoip2/GeoLite2-City.mmdb
+fi
+
+if [ -f "/etc/nginx/geoip2/GeoLite2-Country.mmdb" ]; then
+    wget https://gitlab.com/maurosoft1973-docker/alpine-nginx/-/raw/master/conf/etc/nginx/geoip2/GeoLite2-Country.mmdb -O /etc/nginx/geoip2/GeoLite2-Country.mmdb
+fi
 
 echo "Change default localtime with ${LOCALTIME}"
 cp /usr/share/zoneinfo/${LOCALTIME} /etc/localtime
